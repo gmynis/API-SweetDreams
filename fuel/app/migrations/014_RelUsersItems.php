e7e6e4b61af8f03e7150004b07bd96bc08d5cc67
sergiolp95 <sergiolp1995@gmail.com>
2017-02-21 11:55:48 +0100

    agrego unidades a la tabla del user_items, y  modifico migracion

--- fuel/app/migrations/014_RelUsersItems.php
+++ fuel/app/migrations/014_RelUsersItems.php
@@ -8,8 +8,9 @@ class RelUsersItems
 		\DBUtil::create_table('rel_users_items',array(
 			'fk_users' => array('type' => 'int', 'constraint' => 11),
 			'fk_items' => array('type' => 'int', 'constraint' => 11),
+			'unidades' => array('type' => 'int', 'constraint' => 11),
 
-			), array('fk_users','fk_items'));
+			), array('fk_users','fk_items','unidades'));
 	}
 	function down()
 	{
